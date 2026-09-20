f3be6c23b7938bdf3e5eb6dc6372f42f61989a24
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-20 23:03:14 +0200

    Aggiorna cache PWA LAB 6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lab6-v8";
+const CACHE="lab6-v11";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{

ad609d043be7459000fc9e6ac645e0a0126eb271
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-20 21:58:30 +0200

    Rendi data.json sempre aggiornato

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-const CACHE="lab6-v1";
+const CACHE="lab6-v3";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./sw.js"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  const url=new URL(e.request.url);
+  if(url.pathname.endsWith("/data.json")){
+    e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./data.json")));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html"))));
+});

b36aa09828edcf5a15efc8b5e4e7db746d82b31a
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-22 20:44:46 +0200

    Forza aggiornamento PWA LAB 6 v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lab6-v14";
+const CACHE="lab6-v15";
 const ASSETS=["./","./index.html","./lab-engine.js","./manifest.webmanifest","./data.json"];
 
 async function notify(type,percent,text){

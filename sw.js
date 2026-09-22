8095d49b0363a9bc92461d6bc74da1b8fa6331e2
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-22 20:43:29 +0200

    Forza distribuzione della nuova interfaccia LAB 6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lab6-v13";
+const CACHE="lab6-v14";
 const ASSETS=["./","./index.html","./lab-engine.js","./manifest.webmanifest","./data.json"];
 
 async function notify(type,percent,text){

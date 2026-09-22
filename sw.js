f797f794413957adb73b5593e857ba64f6e1445f
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-22 20:51:42 +0200

    Bump cache LAB 6 v18 per motore v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lab6-v17";
+const CACHE="lab6-v18";
 const ASSETS=["./","./index.html","./lab-engine.js","./manifest.webmanifest","./data.json"];
 
 async function notify(type,percent,text){
